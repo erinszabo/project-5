a2e63851911b41313c0253caf8d62f43ed33fbad
Erin Szabo <eszabo@cocc.edu>
2023-11-13 17:27:19 -0800

    Work on implementing submit and display

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -11,6 +11,9 @@ import acp_times  # Brevet time calculations
 import config
 
 import logging
+import os
+
+from pymongo import MongoClient
 
 ###
 # Globals
@@ -19,10 +22,50 @@ import logging
 app = flask.Flask(__name__)
 CONFIG = config.configuration()
 
+client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
 
+db = client.brevets
+
+collection = db.lists
+
+
+##################################################
+################ MongoDB Functions ############### 
+##################################################
+
+def get_brevet():
+
+    # Get documents (rows) in our collection (table),
+    # Sort by primary key in descending order and limit to 1 document (row)
+    # This will translate into finding the newest inserted document.
+
+    controls = collection.find().sort("_id", -1).limit(1)
+
+    # lists is a PyMongo cursor, which acts like a pointer.
+    # We need to iterate through it, even if we know it has only one entry:
+    for control in controls:
+        
+        return control["brevet"], control["controls"]
+
+
+def submit_brevet(brevet, controls):
+
+    output = collection.insert_one({
+        "brevet": brevet, # the length of the whole brevet
+        "controls": controls}) # I think controls will also be a dictionary
+    _id = output.inserted_id # this is how you obtain the primary key (_id) mongo assigns to your inserted document.
+    
+    return str(_id)
+
+
+##################################################
+################## Flask routes ################## 
+##################################################
 ###
 # Pages
 ###
+
+######### TODO: add routes for submit (POST) and display (GET)
 
 @app.route("/")
 @app.route("/index")
